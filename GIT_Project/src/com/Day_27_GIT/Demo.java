be8472475374b63d44e08844d534742f36fc33aa
Pankaj <Pankaj@LAPTOP-H5UTFHLA>
2022-12-28 15:52:15 +0530

    sonali

---
+++ GIT_Project/src/com/Day_27_GIT/Demo.java
@@ -0,0 +1,11 @@
+package com.Day_27_GIT;
+
+public class Demo {
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello GIT");
+		System.out.println("Hii");
+		System.out.println("");
+	}
+
+}
