2a2af940b7d5e9703f643c5e2a41fde5c0affa5e
spgarad <121485897+spgarad@users.noreply.github.com>
2022-12-28 16:21:55 +0530

    Update Demo.java

--- GIT_Project/src/com/Day_27_GIT/Demo.java
+++ GIT_Project/src/com/Day_27_GIT/Demo.java
@@ -1,6 +1,10 @@
 package com.Day_27_GIT;
 
 public class Demo {
+	public void m1()
+	{
+		System.out.println("m1");
+	}
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello GIT");
